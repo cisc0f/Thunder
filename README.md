1114cdb7835a5ba9cf5e036721f4719ee365f3f3
Lorenzo <lorenzo@coacci.it>
2020-12-26 23:34:21 +0100

    SASS support

---
+++ README.md
@@ -0,0 +1,7 @@
+Using:
+
+Electron
+Typescript instead of Javascript
+Svelte instead of React
+SASS instead of CSS
+Semantic UI instead of Bootrstrap
--- src/App.svelte
+++ src/App.svelte
@@ -1,28 +1,22 @@
-<script>
+<script lang="ts">
 	export let name;
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+<style lang="scss">
+	$color: red;
 	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+	  color: $color;
 	}
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	div {
+	  background: green;
+	  > p {
+		color: #fff;
+	  }
 	}
 </style>
+  
+  <h1>Hello {name}!</h1>
+  
+  <div>
+	<p>SASS is working!</p>
+  </div>
