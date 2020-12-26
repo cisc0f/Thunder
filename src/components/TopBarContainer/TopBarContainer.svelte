13e92af56dff82f41d3413173a7e23402be2891f
Lorenzo <lorenzo@coacci.it>
2020-12-26 23:32:53 +0100

    SASS support

---
+++ src/components/TopBarContainer/TopBarContainer.svelte
@@ -0,0 +1,16 @@
+<!-- TopBarContainer.svelte -->
+<script lang="ts">
+
+</script>
+
+<style lang="sass">
+    
+</style>
+
+<header id="topmenu">
+    <div class="actions">
+        <input placeholder="Type your search term" />
+        <button>Let's find out 🕵️‍♀️</button>
+    </div>
+</header>
+
