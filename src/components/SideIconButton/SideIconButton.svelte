0bf8479fec5ec0e54fa7e09d68782e3b1ae0c843
Lorenzo <lorenzo@coacci.it>
2020-12-30 01:57:05 +0100

    Side Bar Icon Buttons

---
+++ src/components/SideIconButton/SideIconButton.svelte
@@ -0,0 +1,11 @@
+<script>
+    import "./SideIconButton.css";
+    import IconButton from "../IconButton/IconButton.svelte";
+</script>
+
+<style>
+</style>
+
+<div class="side-icon-button">
+    <IconButton />
+</div>
